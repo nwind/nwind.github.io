0b8881704d0a96ba855a0dda2793f295352e15b8
wuduoyi <duoyi.wu@gmail.com>
2014-04-01 11:44:22 +0800

    加上新文章的 RSS

--- generate_feed.py
+++ generate_feed.py
@@ -12,6 +12,16 @@ fg.link(href='http://wuduoyi.com/atom.xml', rel='self')
 fg.description(description = u'Wuduoyi 的个人笔记')
 
 fe = fg.add_entry()
+fe.id('http://wuduoyi.com/note/G-ossip/')
+fe.link(link={'href': 'http://wuduoyi.com/note/G-ossip/'})
+fe.title(u'八卦某 G 的前端开发方式及流程')
+fe.pubdate('2014-4-1 10:00:00 +0800')
+fe.description(description = u'''
+    话说本人从毕业到现在一直在某 B 公司工作，前些年折腾过不少开发方式和工具，但总觉得或许有更好的方案，所以很好奇其它公司内部是如何工作的，我曾经浏览过某 Y 公司内部无所不包的 TWiki，也拜访过某 F 总部了解他们的开发流程，但对某 G 公司却了解不多，只零零碎碎知道一些，这两天抽空梳理了之前收集到的各种资料，希望能给 FEX 后续改进提供参考。
+''')
+fe.updated('2014-4-1 10:00:00 +0800')
+
+fe = fg.add_entry()
 fe.id('http://wuduoyi.com/note/hhvm/')
 fe.link(link={'href': 'http://wuduoyi.com/note/hhvm/'})
 fe.title(u'HHVM 是如何提升 PHP 性能的？')
